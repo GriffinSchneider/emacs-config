5fa561a5b77885c47087b81dfcc34dd3ff6a618b
Griffin Schneider <griffinschneider@gmail.com>
2011-09-23 11:23:00 -0700

    Remove redundant comments in evil-mode-customizations.el.

--- evil-mode-customizations.el
+++ evil-mode-customizations.el
@@ -30,38 +30,19 @@
 (define-key evil-visual-state-map " " (lambda () (interactive) (next-line 10)))
 (define-key evil-normal-state-map (kbd "DEL") (lambda () (interactive) (previous-line 10)))
 
-;; Use <leader>f for find-file
 (define-key evil-normal-state-map "\\f" 'find-file)
-
-;; Use <leader>w for save-buffer
 (define-key evil-normal-state-map "\\w" 'save-buffer)
-
-;; Use <leader>b for buffer-menu
 (define-key evil-normal-state-map "\\b" 'buffer-menu)
 
-;; Use <leader>c for compile
 (define-key evil-normal-state-map "\\c" 'compile)
-
-;; Use <leader>e for next-error
 (define-key evil-normal-state-map "\\e" 'next-error)
-
-;; Use <leader>E for previous-erorr
 (define-key evil-normal-state-map "\\E" 'previous-error)
 
-;; Use <leader>j for ace-jump-mode
 (define-key evil-normal-state-map "\\j" 'ace-jump-mode)
 
-;; Use <leader>x instead of M-x
 (define-key evil-normal-state-map "\\x" 'execute-extended-command)
 
-;; Use <leader>0 instead of C-x 0
 (define-key evil-normal-state-map "\\0" 'delete-window)
-
-;; Use <leader>1 instead of C-x 1
 (define-key evil-normal-state-map "\\1" 'delete-other-windows)
-
-;; Use <leader>2 instead of C-x 2
 (define-key evil-normal-state-map "\\2" 'split-window-vertically)
-
-;; Use <leader>3 instead of C-x 3
 (define-key evil-normal-state-map "\\3" 'split-window-horizontally)
