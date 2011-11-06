4e2f5a49521ea79b3492bac84aabd4ee03bff3db
Griffin Schneider <griffinschneider@gmail.com>
2011-11-06 13:44:30 -0800

    Use "\E" to toggle global-eclim-mode.

--- eclim-customizations.el
+++ eclim-customizations.el
@@ -3,6 +3,7 @@
 (global-eclim-mode)
 (setq eclim-eclipse-dirs '("~/Dev/eclipse"))
 
+(define-key evil-normal-state-map "\\E" 'global-eclim-mode)
 (define-key evil-normal-state-map "\\p" 'eclim-manage-projects)
 
 (provide 'eclim-customizations)
