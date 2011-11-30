351b26ed4119821d7bab78d9970d594dc392d11e
Griffin Schneider <griffinschneider@gmail.com>
2011-11-29 17:12:20 -0800

    Add key-chord, and use key-chord for 'kj' exiting insert mode.

--- evil-mode-customizations.el
+++ evil-mode-customizations.el
@@ -8,23 +8,6 @@
 ;; Make cursor red in Emacs mode.
 (setq evil-emacs-state-cursor '("red" box))
 
-(defun gcs-escape-if-next-char (c)
-  (self-insert-command 1)
-  (let ((next-key (read-event)))
-    (if (= c next-key)
-	(progn
-	  (delete-backward-char 1)
-	  (evil-normal-state))
-        (setq unread-command-events (list next-key)))))
-(defun gcs-escape-if-next-char-is-j (arg)
-  (interactive "p")
-  (if (= arg 1)
-      (gcs-escape-if-next-char ?j)
-      (self-insert-command arg)))
-
-;; make "kj" exit insert mode.
-(define-key evil-insert-state-map (kbd "k") 'gcs-escape-if-next-char-is-j)
-
 ;; Use space and backspace to move up/down 10 lines. 
 (define-key evil-normal-state-map " " (lambda () (interactive) (next-line 10)))
 (define-key evil-visual-state-map " " (lambda () (interactive) (next-line 10)))
