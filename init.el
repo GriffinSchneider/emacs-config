453d907c8281bef661b5346c380201c48bdd5591
Griffin Schneider <griffinschneider@gmail.com>
2012-06-19 17:43:02 -0400

    Show ido completions vertically.

--- init.el
+++ init.el
@@ -54,6 +54,12 @@
 (ido-mode t)
 (setq ido-enable-flex-matching t)
 (setq ido-ignore-buffers (cons "\\*Buffer List\\*" ido-ignore-buffers))
+;; Show ido completions vertically
+(setq ido-decorations (quote ("\n-> " "" "\n   " "\n   ..." "[" "]"
+                              " [No match]" " [Matched]" " [Not readable]"
+                              " [Too big]" " [Confirm]")))
+(defun ido-disable-line-trucation () (set (make-local-variable 'truncate-lines) nil))
+(add-hook 'ido-minibuffer-setup-hook 'ido-disable-line-trucation)
 
 
 (require 'smex)
