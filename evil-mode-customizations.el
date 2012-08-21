980f11c2fb02cab5e95ad895d8e800770b33d5f3
Griffin Schneider <griffinschneider@gmail.com>
2012-08-21 11:17:00 -0400

    Change the way backslash-prefixed keybindings work, so the cursor color can be changed to green while waiting for the key after a backslash prefix.

--- evil-mode-customizations.el
+++ evil-mode-customizations.el
@@ -43,54 +43,103 @@
   ":" 'magit-git-command)
 
 ;; Setup prefix keybindings
+
+;; "\k" kills the buffer without asking and makes sure the buffer menu
+;;  opens with point at the first line.
+(defun gcs-kill-buffer-command ()
+  (interactive)
+  (kill-buffer (current-buffer))
+  (let ((buffer-menu-buffer (get-buffer "*Buffer List*")))
+    (when buffer-menu-buffer
+      (with-current-buffer buffer-menu-buffer
+        (revert-buffer)
+        (gcs-buffer-menu-custom-font-lock)
+        (goto-char (point-min))))))
+
+;; Use \<left> and \<right> to navigate buffer list, ignoring buffer menu
+(defun gcs-previous-buffer ()
+  (interactive)
+  (previous-buffer)
+  (when (string= (buffer-name) "*Buffer List*") (previous-buffer)))
+
+(defun gcs-next-buffer ()
+  (interactive)
+  (next-buffer)
+  (when (string= (buffer-name) "*Buffer List*") (next-buffer)))
+
+(defun comment-dwim-line (&optional arg)
+  "Replacement for the comment-dwim command.
+   If no region is selected and current line is not blank and we are not at the end of the line,
+   then comment current line.
+   Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line."
+  (interactive "*P")
+  (comment-normalize-vars)
+  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+      (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+    (comment-dwim arg)))
+
+(defun gcs-toggle-tab-width-setting ()
+  "Toggle setting tab widths between 4 and 8"
+  (interactive)
+  (setq tab-width (cond ((= tab-width 8) 4)
+                        ((= tab-width 4) 2)
+                        (t 8)))
+  (message (format "Tab width is now %d" tab-width))
+  (redraw-display))
+
+(defun gcs-prefix-key-command ()
+  (interactive)
+  (let ((old-cursor-color (face-background 'cursor))
+        (old-overriding-local-map overriding-local-map))
+    (set-cursor-color "Green")
+    (let* ((overriding-local-map (make-sparse-keymap))
+           (key (read-key-sequence nil))
+           (overriding-local-map old-overriding-local-map))
+      (set-cursor-color old-cursor-color)
+      (mapc (lambda (binding)
+              (when (equal (car binding) key)
+                (call-interactively (cadr binding))))
+            '(("q" quit-window)
+              ("g" magit-status)
+              ("s" sunrise)
+              ("S" sunrise-cd)
+
+              ("u" undo-tree-visualize)
+              ("f" ido-find-file)
+              ("F" ido-find-alternate-file)
+              ("w" save-buffer)
+              ("W" write-file)
+              ("b" buffer-menu)
+              ("v" ido-switch-buffer)
+              ("V" ido-switch-buffer-other-frame)
+
+              ("k" gcs-kill-buffer-command)
+              
+              ("c" compile)
+              ("e" next-error)
+              ("E" previous-error)
+              ("r" eval-buffer)
+              ("j" ace-jump-mode)
+              ("x" smex)
+              ("X" smex-major-mode-commands)
+              ("0" delete-window)
+              ("1" delete-other-windows)
+              ("2" split-window-vertically)
+              ("3" split-window-horizontally)
+
+              ([left]  gcs-previous-buffer)
+              ([right] gcs-next-buffer)
+              ("\\"    comment-dwim-line)
+              ("t"     gcs-toggle-tab-width-setting))))))
+
 (defconst gcs-prefix-key "\\")
 (defconst gcs-prefix-key-maps (list evil-normal-state-map
                                     evil-motion-state-map
                                     evil-emacs-state-map
                                     pianobar-mode-map))
-
-(define-prefix-command 'gcs-prefix-key-map)
-(mapc (lambda (keymap) (define-key keymap gcs-prefix-key 'gcs-prefix-key-map)) gcs-prefix-key-maps)
-(defun gcs-define-prefix-key (key command)
-  (define-key 'gcs-prefix-key-map key command))
-
-(gcs-define-prefix-key "q" 'quit-window)
-(gcs-define-prefix-key "g" 'magit-status)
-(gcs-define-prefix-key "s" 'sunrise)
-(gcs-define-prefix-key "S" 'sunrise-cd)
-
-(gcs-define-prefix-key "u" 'undo-tree-visualize)
-(gcs-define-prefix-key "f" 'ido-find-file)
-(gcs-define-prefix-key "F" 'ido-find-alternate-file)
-(gcs-define-prefix-key "w" 'save-buffer)
-(gcs-define-prefix-key "W" 'write-file)
-(gcs-define-prefix-key "b" 'buffer-menu)
-(gcs-define-prefix-key "v" 'ido-switch-buffer)
-(gcs-define-prefix-key "V" 'ido-switch-buffer-other-frame)
-;; "\k" kills the buffer without asking and makes sure the buffer menu
-;;  opens with point at the first line.
-(gcs-define-prefix-key "k"
- (lambda ()
-   (interactive)
-   (kill-buffer (current-buffer))
-   (let ((buffer-menu-buffer (get-buffer "*Buffer List*")))
-     (when buffer-menu-buffer
-       (with-current-buffer buffer-menu-buffer
-         (revert-buffer)
-         (gcs-buffer-menu-custom-font-lock)
-         (goto-char (point-min)))))))
-
-(gcs-define-prefix-key "c" 'compile)
-(gcs-define-prefix-key "e" 'next-error)
-(gcs-define-prefix-key "E" 'previous-error)
-(gcs-define-prefix-key "r" 'eval-buffer)
-(gcs-define-prefix-key "j" 'ace-jump-mode)
-(gcs-define-prefix-key "x" 'smex)
-(gcs-define-prefix-key "X" 'smex-major-mode-commands)
-(gcs-define-prefix-key "0" 'delete-window)
-(gcs-define-prefix-key "1" 'delete-other-windows)
-(gcs-define-prefix-key "2" 'split-window-vertically)
-(gcs-define-prefix-key "3" 'split-window-horizontally)
+(mapc (lambda (keymap)
+        (define-key keymap gcs-prefix-key 'gcs-prefix-key-command))
+      gcs-prefix-key-maps)
 
 ;; Use j and k pressed within .15 seconds to exit insert mode
 (defun gcs-evil-maybe-exit (entry-key exit-key)
@@ -117,40 +166,6 @@
   (gcs-evil-maybe-exit ?k ?j))
 (define-key evil-insert-state-map "k" 'gcs-evil-maybe-exit-k)
 
-;; Use \<left> and \<right> to navigate buffer list, ignoring buffer menu
-(defun gcs-previous-buffer ()
-  (interactive)
-  (previous-buffer)
-  (when (string= (buffer-name) "*Buffer List*") (previous-buffer)))
-(gcs-define-prefix-key (kbd "<left>") 'gcs-previous-buffer)
-
-(defun gcs-next-buffer ()
-  (interactive)
-  (next-buffer)
-  (when (string= (buffer-name) "*Buffer List*") (next-buffer)))
-(gcs-define-prefix-key (kbd "<right>") 'gcs-next-buffer)
-
-(defun comment-dwim-line (&optional arg)
-  "Replacement for the comment-dwim command.
-   If no region is selected and current line is not blank and we are not at the end of the line,
-   then comment current line.
-   Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line."
-  (interactive "*P")
-  (comment-normalize-vars)
-  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
-      (comment-or-uncomment-region (line-beginning-position) (line-end-position))
-    (comment-dwim arg)))
-(gcs-define-prefix-key "\\" 'comment-dwim-line)
-
-(defun gcs-toggle-tab-width-setting ()
-  "Toggle setting tab widths between 4 and 8"
-  (interactive)
-  (setq tab-width (cond ((= tab-width 8) 4)
-                        ((= tab-width 4) 2)
-                        (t 8)))
-  (message (format "Tab width is now %d" tab-width))
-  (redraw-display))
-(gcs-define-prefix-key "t" 'gcs-toggle-tab-width-setting)
 
 (mapc (lambda (mode) (evil-set-initial-state mode 'emacs))
        '(inferior-emacs-lisp-mode
