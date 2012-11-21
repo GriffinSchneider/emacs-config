63bff6a5483974088109ee7a04cacdc2297d9a84
Griffin Schneider <griffinschneider@gmail.com>
2012-11-21 11:06:52 -0500

    Better syntax highlighting in moonscript-mode.

--- moonscript-mode.el
+++ moonscript-mode.el
@@ -11,25 +11,49 @@
   '("->" "=>" "(" ")" "[" "]" "{" "}"))
 (defvar moonscript-function-regex (regexp-opt moonscript-function-keywords))
 
-;; Assignments - =, +=, -=. etc.
-(defvar moonscript-assignment-regex "\\([-+/*%]\\|\\.\\.\\)?=")
+(defvar moonscript-octal-number-regex
+  "\\_<0x[[:xdigit:]]+\\_>")
 
+(defvar moonscript-table-key-regex
+  "\\_<\\w+:")
+
+(defvar moonscript-ivar-regex
+  "@\\_<\\w+\\_>")
+
+(defvar moonscript-assignment-regex
+  "\\([-+/*%]\\|\\.\\.\\)?=")
+
+(defvar moonscript-number-regex
+  (mapconcat 'identity '("[0-9]+\\.[0-9]*" "[0-9]*\\.[0-9]+" "[0-9]+") "\\|"))
+
+(defvar moonscript-assignment-var-regex
+  (concat "\\(\\_<\\w+\\) = "))
+  
 (defvar moonscript-font-lock-defaults
   (eval-when-compile
-    `((,moonscript-class-name-regex . font-lock-builtin-face)
-      (,moonscript-function-regex   . font-lock-doc-face)
-      (,moonscript-assignment-regex . font-lock-keyword-face)
-      (,moonscript-keywords-regex   . font-lock-keyword-face)
-      ("!"                          . font-lock-warning-face))))
+    `((,moonscript-class-name-regex     . font-lock-type-face)
+      (,moonscript-function-regex       . font-lock-builtin-face)
+      (,moonscript-assignment-regex     . font-lock-preprocessor-face)
+      (,moonscript-keywords-regex       . font-lock-keyword-face)
+      (,moonscript-ivar-regex           . font-lock-variable-name-face)
+      (,moonscript-assignment-var-regex . (1 font-lock-variable-name-face))
+      (,moonscript-octal-number-regex   . font-lock-constant-face)
+      (,moonscript-number-regex         . font-lock-constant-face)
+      (,moonscript-table-key-regex      . font-lock-variable-name-face)
+      ("!"                              . font-lock-warning-face))))
 
 
-
-(define-derived-mode moonscript-mode fundamental-mode
+(define-derived-mode moonscript-mode fundamental-mode  "moonscript"
   (setq font-lock-defaults '(moonscript-font-lock-defaults))
-  (setq mode-name "moonscript")
+  (setq tab-width 4)
+
+  (define-key moonscript-mode-map "<TAB>" (lambda () (interactive) (insert-tab)))
 
   (modify-syntax-entry ?\- ". 12b" moonscript-mode-syntax-table)
-  (modify-syntax-entry ?\n "> b" moonscript-mode-syntax-table))
+  (modify-syntax-entry ?\n "> b" moonscript-mode-syntax-table)
+  (modify-syntax-entry ?\_ "w" moonscript-mode-syntax-table)
+  
+  )
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.moon$" . moonscript-mode))
