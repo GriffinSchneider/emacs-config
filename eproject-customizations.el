2229db8fc647a29bed4ea29598ed578fd365ab07
Griffin Schneider <griffinschneider@gmail.com>
2013-01-30 16:35:34 -0500

    Add more buffers to gcs-buffer-regexps-never-in-projects.

--- eproject-customizations.el
+++ eproject-customizations.el
@@ -1,7 +1,7 @@
 (require 'init)
 
 (defconst gcs-buffer-regexps-never-in-projects
-  '("*Help*" "*Completions*" "*pianobar*"))
+  '("*Help*" "*Completions*" "*pianobar*" "*Ibuffer*" "*Minibuf"))
 
 (defun gcs-buffer-name-excluded-fromp-projects-p (name)
   (some (lambda (r) (string-match r name))
