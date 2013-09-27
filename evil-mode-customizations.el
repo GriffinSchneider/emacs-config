a3ee8253c9a2fae2bb38bac006766de20e9f2946
Griffin Schneider <griffinschneider@gmail.com>
2013-09-27 00:35:42 -0400

    Remove hacks to make ace-jump work with evil visual mode, now that ace-jump supports that itself.

--- evil-mode-customizations.el
+++ evil-mode-customizations.el
@@ -19,42 +19,4 @@
          magit-branch-manager-mode
          pianobar-mode))
 
-;; Use these motions instead of ace-jump's commands
-;; to get ace-jump to work in evil visual mode
-(defmacro gcs-without-evil-visual-hooks (&rest body)
-  `(let ((old-mark (mark)))
-     (remove-hook 'pre-command-hook #'evil-visual-pre-command t)
-     (remove-hook 'post-command-hook #'evil-visual-post-command t)
-     (unwind-protect
-         (progn
-           ,@body
-           (recursive-edit))
-        (add-hook 'pre-command-hook #'evil-visual-pre-command nil t)
-        (add-hook 'post-command-hook #'evil-visual-post-command nil t)
-        (set-mark old-mark))))
-
-(evil-define-motion evil-ace-jump-char-mode (count)
-  :type exclusive
-  (gcs-without-evil-visual-hooks
-   (ace-jump-mode 5)))
-
-(evil-define-motion evil-ace-jump-line-mode (count)
-  :type line
-  (gcs-without-evil-visual-hooks
-   (ace-jump-mode 9)))
-
-(evil-define-motion evil-ace-jump-word-mode (count)
-  :type exclusive
-  (gcs-without-evil-visual-hooks
-   (ace-jump-mode 1)))
-
-(evil-define-motion evil-ace-jump-char-direct-mode (count)
-  :type inclusive
-  (gcs-without-evil-visual-hooks
-   (ace-jump-mode 5)
-   (forward-char 1)))
-
-(add-hook 'ace-jump-mode-end-hook 'exit-recursive-edit)
-
-
 (provide 'evil-mode-customizations)
