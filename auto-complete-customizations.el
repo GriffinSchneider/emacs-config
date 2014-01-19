81088edbf47b7c81d37fccd561d17ceabb867df8
Griffin Schneider <griffinschneider@gmail.com>
2014-01-19 15:46:25 -0500

    Switch to different global auto-complete hack, since mine was apparently broken by 24.4.

--- auto-complete-customizations.el
+++ auto-complete-customizations.el
@@ -12,11 +12,10 @@
 (define-key ac-menu-map (kbd "s-k") 'ac-previous)
 
 ;; dirty fix for having AC everywhere
-(define-globalized-minor-mode real-global-auto-complete-mode
-  auto-complete-mode (lambda ()
-                       (if (not (minibufferp (current-buffer)))
-                         (auto-complete-mode 1))))
-(real-global-auto-complete-mode t)
+(defun auto-complete-mode-maybe ()
+  (unless (minibufferp (current-buffer))
+    (auto-complete-mode 1)))
+(global-auto-complete-mode t)
 
 ;; add the emacs-eclim source
 (add-hook 'eclim-mode-hook (lambda () (setq ac-sources '(ac-source-emacs-eclim ac-source-eclim))))
